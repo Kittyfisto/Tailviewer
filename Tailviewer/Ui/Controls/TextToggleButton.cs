2b833a1c2b5cadeb3ea1e62e668ed6063411b567
Kittyfisto <kittyfisto87@gmail.com>
2016-01-07 18:03:05 +0100

    UI Improvements
    - Added resource dictionary for most used constants
    - Fixed toggle button styles (all flat)

---
+++ Tailviewer/Ui/Controls/TextToggleButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Tailviewer.Ui.Controls
+{
+	public class TextToggleButton : ToggleButton
+	{
+		static TextToggleButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(TextToggleButton), new FrameworkPropertyMetadata(typeof(TextToggleButton)));
+		}
+	}
+}
