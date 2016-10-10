73cb8e62a567337f4ecbb93ec2b817495cf29dc3
Kittyfisto <kittyfisto87@gmail.com>
2016-10-10 02:17:35 +0200

    Code cleanup

--- System.Threading.Tasks.Extensions/Properties/AssemblyInfo.cs
+++ System.Threading.Tasks.Extensions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tasks.Extensions")]
+[assembly: AssemblyTitle("System.Threading.Tasks.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tasks.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("System.Threading.Tasks.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Kittyfisto 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
