42e8a19a6955dd62448d933d1eb926911eb6107d
Kittyfisto <kittyfisto87@gmail.com>
2017-03-16 20:20:13 +0100

    Added more log sample lines

--- LoggerCore/Generator.cs
+++ LoggerCore/Generator.cs
@@ -49,7 +49,16 @@ namespace LoggerCore
 					new LoggingEventTemplate(Level.Debug, "Current value: 3.14159"),
 					new LoggingEventTemplate(Level.Warn, "Unable to parse '3231sass': Expected integer number"),
 					new LoggingEventTemplate(Level.Error, "Caught unexpected exception: System.StackoverflowException"),
-					new LoggingEventTemplate(Level.Fatal, "Wrong universe!")
+					new LoggingEventTemplate(Level.Fatal, "Wrong universe!"),
+					new LoggingEventTemplate(Level.Info, @"Platform assembly: F:\Unity\Editor\Data\Managed\UnityEngine.dll (this message is harmless)"),
+					new LoggingEventTemplate(Level.Debug, "Initialize engine version: 5.5.0f3 (38b4efef76f0)"),
+					new LoggingEventTemplate(Level.Info, @"Setting StandaloneWindows v5.5.0 for Unity v5.5.0f3 to F:\Unity\Editor\Data\PlaybackEngines\windowsstandalonesupport"),
+					new LoggingEventTemplate(Level.Info, "Unloading 38 Unused Serialized files (Serialized files now loaded: 0)"),
+					new LoggingEventTemplate(Level.Info, "[00:00:01] Enlighten: Finished 1 Layout Systems job (0.00s execute, 0.00s integrate, 0.22s wallclock)"),
+					new LoggingEventTemplate(Level.Debug, "Refreshing native plugins compatible for Editor in 0.45 ms, found 4 plugins."),
+					new LoggingEventTemplate(Level.Info, "[00:00:07] Enlighten: Bake started."),
+					new LoggingEventTemplate(Level.Info, "Packing sprites:"),
+					new LoggingEventTemplate(Level.Info, "Preloading 2 native plugins for Editor in 0.13 ms.")
 				};
 		}
 
