d1625c9aaec986a19a2c89b51371cbbb94632f85
Kittyfisto <kittyfisto87@gmail.com>
2017-07-13 20:25:36 +0200

    Introduced attribute to mark defect tests

---
+++ Tailviewer.Test/DefectAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tailviewer.Test
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class DefectAttribute
+		: Attribute
+	{
+		public DefectAttribute(string defectUri)
+		{
+			DefectUri = defectUri;
+		}
+
+		public string DefectUri { get; set; }
+	}
+}
