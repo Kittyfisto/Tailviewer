3e1328383b90d0a2ea2c146dab7a3f2ea00ba5af
Kittyfisto <kittyfisto87@gmail.com>
2017-11-28 20:30:43 +0100

    Added documentation

--- Tailviewer/Settings/IExportSettings.cs
+++ Tailviewer/Settings/IExportSettings.cs
@@ -1,7 +1,13 @@
 ﻿namespace Tailviewer.Settings
 {
+	/// <summary>
+	///     Holds various settings related to exporting data from tailviewer.
+	/// </summary>
 	public interface IExportSettings
 	{
+		/// <summary>
+		///     The absolute path to the folder where all exports are stored in.
+		/// </summary>
 		string ExportFolder { get; set; }
 	}
 }
