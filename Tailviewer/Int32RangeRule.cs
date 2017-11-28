5bf8f3c635b75578a4c310e6abe86b7135b8f7f4
Kittyfisto <kittyfisto87@gmail.com>
2017-11-28 20:51:12 +0100

    Scroll speeds < 1 aren't accepted #126

---
+++ Tailviewer/Int32RangeRule.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Tailviewer
+{
+	/// <summary>
+	/// </summary>
+	/// <remarks>
+	///     TODO: Move to Metrolib
+	/// </remarks>
+	public sealed class Int32RangeRule
+		: ValidationRule
+	{
+		/// <summary>
+		///     The minimum possible value.
+		///     A value only passes validation if it is equal or greater than the given value.
+		/// </summary>
+		public int Minimum { get; set; }
+
+		/// <summary>
+		///     The maximum possible value.
+		///     A value only passes validation if it is equal or less than the given value.
+		/// </summary>
+		public int Maximum { get; set; }
+
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			var content = value as string;
+			if (content != null)
+			{
+				int parsedValue;
+				if (int.TryParse(content, NumberStyles.Integer, cultureInfo, out parsedValue))
+					if (parsedValue >= Minimum && parsedValue <= Maximum)
+						return new ValidationResult(isValid: true, errorContent: null);
+			}
+
+			return new ValidationResult(isValid: false, errorContent: string.Format("Expected an integer between {0} and {1}", Minimum, Maximum));
+		}
+	}
+}
