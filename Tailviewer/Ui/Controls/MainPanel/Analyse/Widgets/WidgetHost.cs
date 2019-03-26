03d2af5aaaed6ef25f5565a77b24652961d6ea71
Kittyfisto <kittyfisto87@gmail.com>
2019-03-26 20:14:56 +0100

    Improved widget presentation Edit/Close button fade in/out on mouse over (previously was a hard "transition")

---
+++ Tailviewer/Ui/Controls/MainPanel/Analyse/Widgets/WidgetHost.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tailviewer.Ui.Controls.MainPanel.Analyse.Widgets
+{
+	public class WidgetHost
+		: Control
+	{
+		static WidgetHost()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(WidgetHost), new FrameworkPropertyMetadata(typeof(WidgetHost)));
+		}
+	}
+}
---
+++ Tailviewer/Ui/Controls/MainPanel/Analyse/Widgets/WidgetHostTitleBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Tailviewer.Ui.Controls.MainPanel.Analyse.Widgets
+{
+	public class WidgetHostTitleBar
+		: Control
+	{
+		static WidgetHostTitleBar()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(WidgetHostTitleBar),
+			                                         new FrameworkPropertyMetadata(typeof(WidgetHostTitleBar)));
+		}
+
+		public WidgetHostTitleBar()
+		{
+			MouseEnter += OnMouseEnter;
+			MouseLeave += OnMouseLeave;
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			UpdateVisualState(false);
+		}
+
+		private void OnMouseEnter(object sender, MouseEventArgs e)
+		{
+			UpdateVisualState();
+		}
+
+		private void OnMouseLeave(object sender, MouseEventArgs e)
+		{
+			UpdateVisualState();
+		}
+
+		private void UpdateVisualState(bool useTransition = true)
+		{
+			VisualStateManager.GoToState(this, IsMouseOver ? "MouseOverState" : "NormalState", useTransition);
+		}
+	}
+}
