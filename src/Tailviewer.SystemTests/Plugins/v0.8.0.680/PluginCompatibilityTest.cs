4b88002015e3358f72612fce5cc7fa11d111f941
Kittyfisto <kittyfisto87@gmail.com>
2019-06-15 15:43:23 +0200

    Added documentation to system tests #187 Systemtests are now executed on AppVeyor

--- src/Tailviewer.SystemTests/Plugins/v0.8.0.680/PluginCompatibilityTest.cs
+++ src/Tailviewer.SystemTests/Plugins/v0.8.0.680/PluginCompatibilityTest.cs
@@ -15,12 +15,14 @@ namespace Tailviewer.SystemTests.Plugins.v0._8._0._680
 		[Test]
 		public void TestLoadFileFormatPlugin()
 		{
+			// CMD> Tailviewer.PluginCreator.exe FileFormatPlugin  Tailviewer.BusinessLogic.Plugins.IFileFormatPlugin 0.8.0.680-beta
 			Load<IFileFormatPlugin>(new Version(0, 8, 0, 680), "FileFormatPlugin.0.0.tvp");
 		}
 
 		[Test]
 		public void TestLoadFileFormatPluginAndLogFileImplementation()
 		{
+			// CMD> Tailviewer.PluginCreator.exe FileFormatPlugin+LogFile  Tailviewer.BusinessLogic.Plugins.IFileFormatPlugin,Tailviewer.BusinessLogic.LogFiles.ILogFile 0.8.0.680-beta
 			Load<IFileFormatPlugin>(new Version(0, 8, 0, 680), "FileFormatPlugin+LogFile.0.0.tvp");
 		}
 
