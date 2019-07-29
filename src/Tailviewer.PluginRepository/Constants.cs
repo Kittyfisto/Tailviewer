9754d822170a78dd9ec84c3e575fd25f0f691d40
Kittyfisto <kittyfisto87@gmail.com>
2019-07-29 14:11:27 +0200

    Changed database location to common app data

--- src/Tailviewer.PluginRepository/Constants.cs
+++ src/Tailviewer.PluginRepository/Constants.cs
@@ -10,6 +10,7 @@ namespace Tailviewer.PluginRepository
 		public static readonly DateTime BuildDate;
 		public static readonly string ApplicationFolder;
 		public static readonly string AppDataLocalFolder;
+		public static readonly string ProgramDataFolder;
 		public static readonly string ApplicationLogFile;
 		public static readonly string ServiceLogFile;
 		public static readonly string PluginDatabaseFilePath;
@@ -21,6 +22,7 @@ namespace Tailviewer.PluginRepository
 			BuildDate = Core.Constants.BuildDate;
 			ApplicationFolder = Core.Constants.ApplicationFolder;
 			AppDataLocalFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationTitle);
+			ProgramDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), ApplicationTitle);
 			ApplicationLogFile = Path.Combine(AppDataLocalFolder, "repository.log");
 			ServiceLogFile = Path.Combine(AppDataLocalFolder, "repository-svc.log");
 			PluginDatabaseFilePath = Path.Combine(AppDataLocalFolder, "Plugins.isdb");
