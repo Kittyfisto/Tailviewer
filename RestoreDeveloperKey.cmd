7c17d8f47c6e38a94db2c3092e03f1cf4c102011
Kittyfisto <kittyfisto87@gmail.com>
2019-10-30 01:29:03 +0100

    Aligned tailviewer public-key references with checked in snk file #217 The tailviewer repository compiles against a developer SNK file by default. All checked in AssemblyInfo.cs files have been corrected to use that key's public key (InternalsVisibleTo). The build server still compiles against another secret SNK file. In order to facilitate this, before compiling, the official key is restored and all relevant AssemblyInfo.cs files are patched via the new AssemblyInfo.exe build-tool.
    The tool's source code can be found at https://github.com/Kittyfisto/BuildTools.

---
+++ RestoreDeveloperKey.cmd
@@ -0,0 +1,51 @@
+@setlocal EnableDelayedExpansion
+@echo off
+
+set SCRIPT_DIR=%~dp0
+set SRC_DIR=%SCRIPT_DIR%src
+set ASSEMBLYINFO_TOOL=%SCRIPT_DIR%tools\AssemblyInfo.exe
+set DEVELOPER_KEY=%SCRIPT_DIR%sig\DeveloperKey.snk
+set CURRENT_KEY=%SCRIPT_DIR%sig\Key.snk
+set "TAILVIEWER_ASSEMBLIES=Installer Tailviewer Tailviewer.AcceptanceTests Tailviewer.Api archiver Tailviewer.Archiver.Test Tailviewer.Core Tailviewer.PluginRepository Tailviewer.PluginRepository.Service Tailviewer.PluginRepository.Test Tailviewer.SystemTests Tailviewer.Test"
+
+echo Restoring developer key (strong name key file)...
+
+if not exist "%DEVELOPER_KEY%" goto :DEVELOPER_KEY_MISSING
+
+fc /B "%DEVELOPER_KEY%" "%CURRENT_KEY%">nul
+if %errorlevel% == 0 goto :NO_RESTORE_NECESSARY
+
+echo Modifying AssemblyInfo.cs files of the entire solution...
+for /r "%SRC_DIR%" %%i in (*.*) do call :CHANGE_ASSEMBLYINFO %%i || goto :ASSEMBLYINFO_ERROR
+
+echo Copying developer key to current key file...
+COPY "%DEVELOPER_KEY%" "%CURRENT_KEY%" || goto :COPY_ERROR
+
+echo SUCESS: Tailviewer now compiles with DeveloperKey.snk
+exit /b 0
+
+:DEVELOPER_KEY_MISSING
+echo ERROR: Developer key %DEVELOPER_KEY% is missing or cannot be accessed.
+exit /b -1
+
+:NO_RESTORE_NECESSARY
+echo Developer key has already been restored, nothing needs to be done
+exit /b 0
+
+:COPY_ERROR
+echo ERROR: There was an error copying the developer key: copy returned %errorlevel%
+exit /b -3
+
+:CHANGE_ASSEMBLYINFO
+set ASSEMBLY_INFO_FULL_PATH=%~1
+set ASSEMBLY_INFO_FILE_NAME=%~n1%~x1
+if not %ASSEMBLY_INFO_FILE_NAME% == AssemblyInfo.cs exit /b 0
+echo Patching %ASSEMBLY_INFO_FULL_PATH%...
+"%ASSEMBLYINFO_TOOL%" patch-internals-visible-to --assembly-info "%ASSEMBLY_INFO_FULL_PATH%" --key-path "%DEVELOPER_KEY%" --assemblies %TAILVIEWER_ASSEMBLIES%
+exit /b %errorlevel%
+
+:ASSEMBLYINFO_ERROR
+echo ERROR: There was an error modifying AssemblyInfo.cs: AssemblyInfo.exe returned %errorlevel%
+exit /b -4
+
+endlocal
---
+++ RestoreOfficialKey.cmd
@@ -0,0 +1,62 @@
+@setlocal EnableDelayedExpansion
+@echo off
+
+set SCRIPT_DIR=%~dp0
+set SRC_DIR=%SCRIPT_DIR%src
+set ASSEMBLYINFO_TOOL=%SCRIPT_DIR%tools\AssemblyInfo.exe
+set CRYPT_TOOL=%SCRIPT_DIR%tools\Crypt.exe
+set OFFICIAL_KEY=%SCRIPT_DIR%sig\sns
+set DEVELOPER_KEY=%SCRIPT_DIR%sig\DeveloperKey.snk
+set CURRENT_KEY=%SCRIPT_DIR%sig\Key.snk
+set "TAILVIEWER_ASSEMBLIES=Installer Tailviewer Tailviewer.AcceptanceTests Tailviewer.Api archiver Tailviewer.Archiver.Test Tailviewer.Core Tailviewer.PluginRepository Tailviewer.PluginRepository.Service Tailviewer.PluginRepository.Test Tailviewer.SystemTests Tailviewer.Test"
+
+echo Restoring official key (strong name key file)...
+
+if not exist "%OFFICIAL_KEY%" goto :OFFICIAL_KEY_MISSING
+if not exist "%CURRENT_KEY%" goto :START
+
+rem We assume that if the current key and the official key are different, then we have restored the official key.
+rem The reason being is that we do not want to CONSTANTLY decrypt the official key to the harddisk (for obvious reasons).
+fc /B "%DEVELOPER_KEY%" "%CURRENT_KEY%">nul
+if %errorlevel% == 1 goto :NO_RESTORE_NECESSARY
+
+:START
+
+echo Decrypting official key...
+"%CRYPT_TOOL%" decrypt "%OFFICIAL_KEY%" "%CURRENT_KEY%" || goto :DECRYPT_ERROR
+
+echo Modifying AssemblyInfo.cs files of the entire solution...
+for /r "%SRC_DIR%" %%i in (*.*) do call :CHANGE_ASSEMBLYINFO %%i || goto :ASSEMBLYINFO_ERROR
+
+echo SUCESS: Tailviewer now compiles with the official strong name key
+exit /b 0
+
+:OFFICIAL_KEY_MISSING
+echo ERROR: Official key %OFFICIAL_KEY% is missing or cannot be accessed.
+exit /b -1
+
+:NO_RESTORE_NECESSARY
+echo Official key has already been restored, nothing needs to be done
+exit /b 0
+
+:DECRYPT_ERROR
+echo ERROR: Unable to decrypt official key
+exit /b -2
+
+:ASSEMBLYINFO_ERROR
+echo ERROR: There was an error modifying AssemblyInfo.cs: AssemblyInfo.exe returned %errorlevel%
+exit /b -3
+
+:COPY_ERROR
+echo ERROR: There was an error copying the developer key: copy returned %errorlevel%
+exit /b -4
+
+:CHANGE_ASSEMBLYINFO
+set ASSEMBLY_INFO_FULL_PATH=%~1
+set ASSEMBLY_INFO_FILE_NAME=%~n1%~x1
+if not %ASSEMBLY_INFO_FILE_NAME% == AssemblyInfo.cs exit /b 0
+echo Patching %ASSEMBLY_INFO_FULL_PATH%...
+"%ASSEMBLYINFO_TOOL%" patch-internals-visible-to --assembly-info "%ASSEMBLY_INFO_FULL_PATH%" --key-path "%CURRENT_KEY%" --assemblies %TAILVIEWER_ASSEMBLIES%
+exit /b %errorlevel%
+
+endlocal
