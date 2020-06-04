86b28f35bcb99130231e65975dff2a7db78513b1
Kittyfisto <kittyfisto87@gmail.com>
2020-06-04 13:49:59 +0200

    All projects now reference GlobalAssemblyInfo.cs

--- src/Tailviewer.Api/Properties/AssemblyInfo.cs
+++ src/Tailviewer.Api/Properties/AssemblyInfo.cs
@@ -20,17 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0c18b216-9ff4-4dcf-88c0-4b1448892f43")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
-[assembly: AssemblyInformationalVersion("0.8.0.0")]
