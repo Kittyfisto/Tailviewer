2ab439452915748cfa9a7cd8aa97ba5daabc0c29
Kittyfisto <kittyfisto87@gmail.com>
2021-02-26 21:31:23 +0100

    Redesigned central log level filter buttons (probably not final, but at least usable compared to before)

---
+++ src/Tailviewer/Ui/Controls/LogView/LogLevelToggleButton.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Metrolib;
+using Metrolib.Controls;
+
+namespace Tailviewer.Ui.Controls.LogView
+{
+	/// <summary>
+	///     Quickly filters / shows log entries with the specified log level.
+	/// </summary>
+	public sealed class LogLevelToggleButton
+		: FlatToggleButton
+	{
+		public static readonly DependencyProperty LogLevelProperty = DependencyProperty.Register(
+		 "LogLevel", typeof(LevelFlags), typeof(LogLevelToggleButton),
+		 new PropertyMetadata(default(LevelFlags), OnLogLevelChanged));
+
+		private static readonly DependencyPropertyKey IconOutlinePropertyKey
+			= DependencyProperty.RegisterReadOnly("IconOutline", typeof(Geometry), typeof(LogLevelToggleButton),
+			                                      new FrameworkPropertyMetadata(default(Geometry),
+				                                                                    FrameworkPropertyMetadataOptions
+					                                                                    .None));
+
+		public static readonly DependencyProperty IconOutlineProperty
+			= IconOutlinePropertyKey.DependencyProperty;
+
+		private static readonly DependencyPropertyKey IconBackgroundPropertyKey
+			= DependencyProperty.RegisterReadOnly("IconBackground", typeof(Geometry), typeof(LogLevelToggleButton),
+			                                      new FrameworkPropertyMetadata(default(Geometry),
+				                                                                    FrameworkPropertyMetadataOptions
+					                                                                    .None));
+
+		public static readonly DependencyProperty IconBackgroundProperty
+			= IconBackgroundPropertyKey.DependencyProperty;
+
+		public static readonly DependencyProperty IconBackgroundBrushProperty = DependencyProperty.Register(
+		 "IconBackgroundBrush", typeof(Brush), typeof(LogLevelToggleButton), new PropertyMetadata(default(Brush)));
+
+		static LogLevelToggleButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(LogLevelToggleButton),
+			                                         new FrameworkPropertyMetadata(typeof(LogLevelToggleButton)));
+		}
+
+		public Brush IconBackgroundBrush
+		{
+			get { return (Brush) GetValue(IconBackgroundBrushProperty); }
+			set { SetValue(IconBackgroundBrushProperty, value); }
+		}
+
+		public Geometry IconBackground
+		{
+			get { return (Geometry) GetValue(IconBackgroundProperty); }
+			protected set { SetValue(IconBackgroundPropertyKey, value); }
+		}
+
+		public Geometry IconOutline
+		{
+			get { return (Geometry) GetValue(IconOutlineProperty); }
+			protected set { SetValue(IconOutlinePropertyKey, value); }
+		}
+
+		public LevelFlags LogLevel
+		{
+			get { return (LevelFlags) GetValue(LogLevelProperty); }
+			set { SetValue(LogLevelProperty, value); }
+		}
+
+		private static void OnLogLevelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((LogLevelToggleButton) d).OnLogLevelChanged((LevelFlags) e.NewValue);
+		}
+
+		private void OnLogLevelChanged(LevelFlags logLevel)
+		{
+			IconOutline = GetIconOutline(logLevel);
+			IconBackground = GetIconBackground(logLevel);
+		}
+
+		private Geometry GetIconOutline(LevelFlags logLevel)
+		{
+			switch (logLevel)
+			{
+				case LevelFlags.Other: return null;
+				case LevelFlags.Trace: return null;
+				case LevelFlags.Debug: return Icons.BugOutline;
+				case LevelFlags.Info: return Icons.InformationOutline;
+				case LevelFlags.Warning: return Icons.AlertOutline;
+				case LevelFlags.Error: return Icons.AlertCircleOutline;
+				case LevelFlags.Fatal: return Icons.AlertOctagonOutline;
+				case LevelFlags.All: return null;
+				default:
+					return null;
+			}
+		}
+
+		private Geometry GetIconBackground(LevelFlags logLevel)
+		{
+			switch (logLevel)
+			{
+				case LevelFlags.Other: return null;
+				case LevelFlags.Trace: return null;
+				case LevelFlags.Debug: return Icons.Bug;
+				case LevelFlags.Info: return Icons.Information;
+				case LevelFlags.Warning: return Icons.Alert;
+				case LevelFlags.Error: return Icons.AlertCircle;
+				case LevelFlags.Fatal: return Icons.AlertOctagon;
+				case LevelFlags.All: return null;
+				default:
+					return null;
+			}
+		}
+	}
+}
