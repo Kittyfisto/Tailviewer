68520685dc3ac8a563b8eefefeed3b42da2cdc8d
Kittyfisto <kittyfisto87@gmail.com>
2021-02-28 12:29:56 +0100

    Added context sensitive tooltips to central toolbar

---
+++ src/Tailviewer/Ui/Converters/FollowTailTooltipConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Tailviewer.Ui.Converters
+{
+	public sealed class FollowTailTooltipConverter
+		: IValueConverter
+	{
+		#region Implementation of IValueConverter
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool isChecked))
+				return null;
+
+			if (isChecked)
+				return "Click to stop following the tail of this data source";
+
+			return "Click to follow the tail of this data source";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ src/Tailviewer/Ui/Converters/LevelToggleTooltipConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Tailviewer.Ui.Converters
+{
+	public sealed class LevelToggleTooltipConverter
+		: IValueConverter
+	{
+		public LevelFlags Level { get; set; }
+
+		#region Implementation of IValueConverter
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool isChecked))
+				return null;
+
+			return string.Format("Click to {0} {1} log entries", isChecked ? "hide" : "show", Level);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
