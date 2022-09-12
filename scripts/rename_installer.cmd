858f1c402f532e09d2c0fb434b6335bbc2f330be
Simon Mießler <simon.miessler@gmx.net>
2022-09-12 23:44:39 +0200

    Build scripts create portable archive (#359)
    Every Tailviewer build (both mainline and MRs) creates a "portable" version which is merely a zipped version of the installation folder under "%PROGRAMFILES%\Tailviewer". This version should be executable directly from a USB drive since it doesn't write to the installation directory.
    
    However there is still the major drawback that the computer needs to have .NET 4.7.1 installed, which is why this isn't a truly portable version. That will be addressed once Tailviewer has been ported to >= .NET 5.

--- scripts/rename_installer.cmd
+++ scripts/rename_installer.cmd
@@ -19,13 +19,26 @@ SET source_installer_name="Tailviewer-setup.exe"
 
 if [%BRANCH_NAME%] == [master] (
 	if [%APPVEYOR_BUILD_VERSION%] == [] goto :NO_VERSION
-	SET dest_installer_name="Tailviewer-setup-%APPVEYOR_BUILD_VERSION%.exe"
+	set dest_portable_name=Tailviewer-portable-%APPVEYOR_BUILD_VERSION%.zip
+	set dest_installer_name=Tailviewer-setup-%APPVEYOR_BUILD_VERSION%.exe
 ) else (
-	SET dest_installer_name="Tailviewer-setup-branch-%BRANCH_NAME%-%COMMIT_HASH%.exe"
+	set dest_portable_name=Tailviewer-portable-branch-%BRANCH_NAME%-%COMMIT_HASH%.zip
+	set dest_installer_name=Tailviewer-setup-branch-%BRANCH_NAME%-%COMMIT_HASH%.exe
 )
 
 echo Renaming installer to %dest_installer_name%
-ren %source_installer_name% %dest_installer_name% || goto :MOVE_ERROR
+ren %source_installer_name% "%dest_installer_name%" || goto :MOVE_ERROR
+
+echo Executing installer silently....
+call "%dest_installer_name%" silentinstall || goto :INSTALLATION_FAILED
+echo Installation succeeded!
+
+set install_directory=%PROGRAMFILES%\Tailviewer
+set portable_full_name=%cd%\%dest_portable_name%
+echo Creating portable version from %install_directory% to %portable_full_name%
+tar -a -cvf "%portable_full_name%" -C "%install_directory%" * || goto :PORTABLE_FAILED
+echo Successfully created portable archive!
+
 exit /b 0
 
 :NO_VERSION
@@ -35,5 +48,13 @@ exit /b -1
 :MOVE_ERROR
 echo ERROR: Unable to rename installer, ren returned %errorlevel%
 exit /b -2
+
+:INSTALLATION_FAILED
+echo ERROR: Installation failed, setup returned %errorlevel%
+exit /b -3
+
+:PORTABLE_FAILED
+echo ERROR: Creating zip archive from installation failed, tar returned %errorlevel% 
+exit /b -4
 
 endlocal
